feff199ef2ccaee3266c8c09c813b3274d6480d9
Muhammad Afif Asyraaf <85404133+acaphilos@users.noreply.github.com>
2025-01-15 16:22:49 +0800

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'smtp-relay.brevo.com';
-$config['smtp_user'] = 'afifasyraf52@gmail.com';
-$config['smtp_pass'] = 'fSrwyFE7GgdcOp4H';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
 $config['smtp_port'] = 587; // Adjust the port as needed
